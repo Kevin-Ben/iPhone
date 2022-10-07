e55e73aa5b999a49965c39bce6af85e19762e121
陈德华 <hua@131.im>
2022-10-07 17:10:10 +0800

    Update ssb.js

--- ssb.js
+++ ssb.js
@@ -6,80 +6,17 @@ var cc = new Date(curDate.getTime() - 24 * 60 * 60 * 1000 * 4);
 var bb = new Date(curDate.getTime() - 24 * 60 * 60 * 1000 * 6);
 var aa = new Date(curDate.getTime() - 24 * 60 * 60 * 1000 * 8);
 
-var treeMap = [
-    {
-        "test_orgname": "上海中检医学检验所（气膜）",
-        "sample_date": "15:08:50",
-        "collect_date": "18:06:21",
-        "report_date": "20:28:11",
-    },
-    {
-        "test_orgname": "上海中检医学检验所（气膜）",
-        "sample_date": "09:17:03",
-        "collect_date": "11:54:53",
-        "report_date": "16:00:42",
-    },
-    {
-        "test_orgname": "上海中检医学检验所（气膜）",
-        "sample_date": "09:22:15",
-        "collect_date": "11:56:47",
-        "report_date": "16:15:03",
-    },
-    {
-        "test_orgname": "上海中检医学检验所（气膜）",
-        "sample_date": "16:03:08",
-        "collect_date": "18:11:05",
-        "report_date": "20:18:12",
-    },
-    {
-        "test_orgname": "上海解码医学检验所（气膜实验室）",
-        "sample_date": "15:36:25",
-        "collect_date": "18:09:02",
-        "report_date": "20:32:15",
-    }
-]
-
-var tpl = {
-    "nat_result_name": "阴性",
-    "check_project": "核酸",
-    "sample_type_name": "咽拭子",
-    "sample_date": "",
-    "nat_result": "1",
-    "test_orgname": "",
-    "collect_date": "",
-    "card_type_name": "身份证",
-    "collect_mode": "1",
-    "sample_orgname": "",
-    "name": "陈德华",
-    "report_date": ""
-}
-
 var data = []
 
-var i = 0
-
-for (k in treeMap) {
-    i = k + 1
-    data[k] = tpl
-    data[k]["test_orgname"] = treeMap[k]["test_orgname"]
-    data[k]["sample_date"] = showLocalTime(new Date(curDate.getTime() - 24 * 60 * 60 * 1000 * i), treeMap[k]["sample_date"])
-    data[k]["collect_date"] = showLocalTime(new Date(curDate.getTime() - 24 * 60 * 60 * 1000 * i), treeMap[k]["collect_date"])
-    data[k]["report_date"] = showLocalTime(new Date(curDate.getTime() - 24 * 60 * 60 * 1000 * i), treeMap[k]["report_date"])
-}
-
 function showLocalTime(date, time) {
-
     if (date.getMonth() < 12) {
         return date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + time
     }
-
     return date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate() + " " + time
-
 }
 
 if (body) {
     var obj = JSON.parse($response.body)
-    // obj.data = data
     obj.data = [
         {
             "nat_result_name": "阴性",
