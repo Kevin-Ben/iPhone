285052e7ffc41d20f42993dc71a6dfb854c6a46e
陈德华 <hua@131.im>
2022-10-07 13:34:20 +0800

    Update akm.js

--- akm.js
+++ akm.js
@@ -1,16 +1,10 @@
+var body = $response.body
+var url = $request.url
 var curDate = new Date();
 var dd = new Date(curDate.getTime() - 24 * 60 * 60 * 1000);
 var cc = new Date(curDate.getTime() - 24 * 60 * 60 * 1000 * 2);
 var bb = new Date(curDate.getTime() - 24 * 60 * 60 * 1000 * 4);
 var aa = new Date(curDate.getTime() - 24 * 60 * 60 * 1000 * 6);
-
-function getMonth(month) {
-    if (month == 12) {
-        return month
-    }
-    return (month + 1)
-}
-
 var resp = {
     "flag": true,
     "data": {
@@ -18,13 +12,13 @@ var resp = {
             {
                 "id": "33AFCF9FB4C34D59BFDB424D375C111A",
                 "hsjcjgmc": "庐江县人民医院东区",
-                "hscjsj": dd.getFullYear() + "-" + getMonth(dd.getMonth()) + "-" + dd.getDate() + " 08:58:36",
+                "hscjsj": dd.getFullYear() + "-" + dd.getMonth() + "-" + dd.getDate() + " 08:58:36",
                 "sourceSys": null,
                 "hsjcjg": "阴性",
                 "dataSource": "1",
-                "hsjcsj": dd.getFullYear() + "-" + getMonth(dd.getMonth()) + "-" + dd.getDate() + " 20:28:11",
+                "hsjcsj": dd.getFullYear() + "-" + dd.getMonth() + "-" + dd.getDate() + " 20:28:11",
                 "isDelete": "**华",
-                "createTime": dd.getFullYear() + getMonth(dd.getMonth()) + dd.getDate() + "4322",
+                "createTime": dd.getFullYear() + dd.getMonth() + dd.getDate() + "4322",
                 "isValid": "3426**********6475",
                 "hscjjgmc": "泥河镇便民核酸采集点",
                 "name": "7c8501ec43a2aeeb723ad1d8c1d8d31c",
@@ -33,13 +27,13 @@ var resp = {
             {
                 "id": "F91B4FEFF67118DC4CAF8AA98786500C",
                 "hsjcjgmc": "庐江县人民医院东区",
-                "hscjsj": cc.getFullYear() + "-" + getMonth(cc.getMonth()) + "-" + cc.getDate() + " 09:15:10",
+                "hscjsj": cc.getFullYear() + "-" + cc.getMonth() + "-" + cc.getDate() + " 09:15:10",
                 "sourceSys": null,
                 "hsjcjg": "阴性",
                 "dataSource": "1",
-                "hsjcsj": cc.getFullYear() + "-" + getMonth(cc.getMonth()) + "-" + cc.getDate() + " 20:13:16",
+                "hsjcsj": cc.getFullYear() + "-" + cc.getMonth() + "-" + cc.getDate() + " 20:13:16",
                 "isDelete": "**华",
-                "createTime": dd.getFullYear() + getMonth(cc.getMonth()) + dd.getDate + "0117",
+                "createTime": dd.getFullYear() + dd.getMonth() + dd.getDate + "0117",
                 "isValid": "3426**********6475",
                 "hscjjgmc": "泥河镇便民核酸采集点",
                 "name": "7c8501ec43a2aeeb723ad1d8c1d8d31c",
@@ -48,13 +42,13 @@ var resp = {
             {
                 "id": "1710DB56E14DA0F7C9CF5FA0AE0B839E",
                 "hsjcjgmc": "庐江县人民医院东区",
-                "hscjsj": bb.getFullYear() + "-" + getMonth(bb.getMonth()) + "-" + bb.getDate() + " 08:15:17",
+                "hscjsj": bb.getFullYear() + "-" + bb.getMonth() + "-" + bb.getDate() + " 08:15:17",
                 "sourceSys": null,
                 "hsjcjg": "阴性",
                 "dataSource": "1",
-                "hsjcsj": bb.getFullYear() + "-" + getMonth(bb.getMonth()) + "-" + bb.getDate() + " 19:45:31",
+                "hsjcsj": bb.getFullYear() + "-" + bb.getMonth() + "-" + bb.getDate() + " 19:45:31",
                 "isDelete": "**华",
-                "createTime": bb.getFullYear() + getMonth(bb.getMonth()) + bb.getDate() + "0016",
+                "createTime": bb.getFullYear() + bb.getMonth() + bb.getDate() + "0016",
                 "isValid": "3426**********6475",
                 "hscjjgmc": "泥河镇便民核酸采集点",
                 "name": "7c8501ec43a2aeeb723ad1d8c1d8d31c",
@@ -63,13 +57,13 @@ var resp = {
             {
                 "id": "84017728ECD54F5395C94F314F361B56",
                 "hsjcjgmc": "庐江县人民医院东区",
-                "hscjsj": aa.getFullYear() + "-" + getMonth(aa.getMonth()) + "-" + aa.getDate() + " 10:20:42",
+                "hscjsj": aa.getFullYear() + "-" + aa.getMonth() + "-" + aa.getDate() + " 10:20:42",
                 "sourceSys": null,
                 "hsjcjg": "阴性",
                 "dataSource": "1",
-                "hsjcsj": aa.getFullYear() + "-" + getMonth(aa.getMonth()) + "-" + aa.getDate() + " 20:19:22",
+                "hsjcsj": aa.getFullYear() + "-" + aa.getMonth() + "-" + aa.getDate() + " 20:19:22",
                 "isDelete": "**华",
-                "createTime": aa.getFullYear() + getMonth(aa.getMonth()) + aa.getDate() + "0016",
+                "createTime": aa.getFullYear() + aa.getMonth() + aa.getDate() + "0016",
                 "isValid": "3426**********6475",
                 "hscjjgmc": "泥河镇便民核酸采集点",
                 "name": "7c8501ec43a2aeeb723ad1d8c1d8d31c",
