1a66995c2425159335edaee9cee86eb268c1f226
陈德华 <hua@131.im>
2022-11-14 10:37:17 +0800

    * update
    * update

--- ah/wst.js
+++ ah/wst.js
@@ -5,6 +5,7 @@ var dd = new Date(curDate.getTime() - 24 * 60 * 60 * 1000);
 if (body) {
     var obj = JSON.parse($response.body)
     obj.data.hsInfo = {
+        "hsTime" : 24,
         "hsjg": "阴性",
         "hsDate": dd.getFullYear() + "-" + (dd.getMonth() + 1) + "-" + dd.getDate() + " 20:15:36",
     }
